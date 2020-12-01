d56fca8fe8de968a760e2de5a2d8fd696d99a009
Saxon Jensen <saxon.jensen@healthengine.com.au>
2020-12-01 17:30:57 +0800

    Let's get going

--- src/Advent/Y2020/Day1/Part1.hs
+++ src/Advent/Y2020/Day1/Part1.hs
@@ -1,4 +1,22 @@
 module Advent.Y2020.Day1.Part1 where
 
+import Data.List
+import Data.Maybe
+import Relude.Unsafe (read)
+import Prelude hiding (lines)
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  input <- readFile "src/Advent/Y2020/Day1/input"
+  print $ solve $ lines input
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x, y) | (x, ix) <- zip xs [0 ..], (y, iy) <- zip ys [0 ..], ix /= iy]
+
+solve :: [String] -> Int
+solve elements = x * y
+  where
+    nums = map read elements :: [Int]
+    cart = cartProd nums nums
+    cond = \(a, b) -> a + b == 2020
+    (x, y) = fromJust $ find cond cart
--- src/Advent/Y2020/Day1/Part2.hs
+++ src/Advent/Y2020/Day1/Part2.hs
@@ -1,4 +1,22 @@
 module Advent.Y2020.Day1.Part2 where
 
+import Data.List
+import Data.Maybe
+import Relude.Unsafe (read)
+import Prelude hiding (lines)
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  input <- readFile "src/Advent/Y2020/Day1/input"
+  print $ solve $ lines input
+
+triples :: [a] -> [b] -> [c] -> [(a, b, c)]
+triples xs ys zs = [(x, y, z) | (x, ix) <- zip xs [0 ..], (y, iy) <- zip ys [0 ..], (z, iz) <- zip zs [0 ..], ix /= iy && ix /= iz]
+
+solve :: [String] -> Int
+solve elements = x * y * z
+  where
+    nums = map read elements :: [Int]
+    trips = triples nums nums nums
+    cond = \(a, b, c) -> a + b + c == 2020
+    (x, y, z) = fromJust $ find cond trips

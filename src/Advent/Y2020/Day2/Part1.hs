07fcd7ea8c1f064b0a93ee57242f99f56b755384
Saxon Jensen <saxon.jensen@healthengine.com.au>
2020-12-02 21:25:26 +0800

    Hell yeah another question done

--- src/Advent/Y2020/Day2/Part1.hs
+++ src/Advent/Y2020/Day2/Part1.hs
@@ -1,4 +1,28 @@
 module Advent.Y2020.Day2.Part1 where
 
+import Advent.Input
+import Data.Text (unpack)
+import Text.Scanf
+
+ruleFormat = fmt_ (int . "-" % int . " " % char . ": " % string)
+
+parseRule :: String -> (Int, Int, Char, String)
+parseRule rule = (min, max, ch, str)
+  where
+    Just (min :+ max :+ ch :+ str :+ ()) = scanf ruleFormat rule
+
+count :: Char -> String -> Int
+count c cs = foldr (\x acc -> if x == c then acc + 1 else acc) 0 cs
+
+checkRule :: (Int, Int, Char, String) -> Bool
+checkRule (min, max, char, str) = cnt <= max && cnt >= min
+  where
+    cnt = count char str
+
+solve :: [Text] -> Int
+solve = length . (filter checkRule) . (map (parseRule . unpack))
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  lines <- readInput "src/Advent/Y2020/Day2/input"
+  print $ solve lines
--- src/Advent/Y2020/Day2/Part2.hs
+++ src/Advent/Y2020/Day2/Part2.hs
@@ -1,4 +1,22 @@
 module Advent.Y2020.Day2.Part2 where
 
+import Advent.Input
+import Advent.Y2020.Day2.Part1 (parseRule)
+import Data.Text (unpack)
+import Relude.Unsafe ((!!))
+
+checkRule :: (Int, Int, Char, String) -> Bool
+checkRule (p1, p2, char, str) = con1 || con2
+  where
+    x = str !! (p1 - 1)
+    y = str !! (p2 - 1)
+    con1 = x == char && y /= char
+    con2 = x /= char && y == char
+
+solve :: [Text] -> Int
+solve = length . (filter checkRule) . (map (parseRule . unpack))
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  lines <- readInput "src/Advent/Y2020/Day2/input"
+  print $ solve lines

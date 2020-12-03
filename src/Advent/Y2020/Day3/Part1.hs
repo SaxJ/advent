f99db6bc30392db58298b2d5b1b3a005fa842e95
Saxon Jensen <saxon.jensen@healthengine.com.au>
2020-12-03 14:55:00 +0800

    Heck yeah day 3

--- src/Advent/Y2020/Day3/Part1.hs
+++ src/Advent/Y2020/Day3/Part1.hs
@@ -1,4 +1,23 @@
 module Advent.Y2020.Day3.Part1 where
 
+import Advent.Input (readInput)
+import Data.Text (unpack)
+import Relude.Unsafe
+
+solve :: [String] -> Int
+solve = length . hits . positions
+
+positions :: [String] -> [(Int, String)]
+positions strs = map (\(p, s) -> (p `mod` length s, s)) pairs
+  where
+    pairs = zip [0, 3 ..] strs
+
+hits :: [(Int, String)] -> [(Int, String)]
+hits = filter check
+  where
+    check (p, s) = s !! p == '#'
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  lines <- readInput "src/Advent/Y2020/Day3/input"
+  print $ solve $ map unpack lines
--- src/Advent/Y2020/Day3/Part2.hs
+++ src/Advent/Y2020/Day3/Part2.hs
@@ -1,4 +1,27 @@
 module Advent.Y2020.Day3.Part2 where
 
+import Advent.Input (readInput)
+import Data.Text (unpack)
+import Relude.Unsafe
+
+solve :: [String] -> Int
+solve = product . map (length . hits . normalise) . pairsLists
+
+normalise :: [(Int, String)] -> [(Int, String)]
+normalise = map (\(p, s) -> (p `mod` length s, s))
+
+pairsLists :: [String] -> [[(Int, String)]]
+pairsLists strs = [zip [0, 1 ..] strs, zip [0, 3 ..] strs, zip [0, 5 ..] strs, zip [0, 7 ..] strs, zip [0, 1 ..] evens]
+  where
+    indexes = zip strs ([0 ..] :: [Int])
+    evens = map fst $ filter (\(_, i) -> even i) indexes
+
+hits :: [(Int, String)] -> [(Int, String)]
+hits = filter check
+  where
+    check (p, s) = s !! p == '#'
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  lines <- readInput "src/Advent/Y2020/Day3/input"
+  print $ solve $ map unpack lines

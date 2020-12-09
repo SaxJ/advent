405c304509bccd64ac6f5758adfaf6dbfa52525a
Saxon Jensen <saxon.jensen@healthengine.com.au>
2020-12-09 11:27:12 +0800

    Day 6

--- src/Advent/Y2020/Day6/Part1.hs
+++ src/Advent/Y2020/Day6/Part1.hs
@@ -1,4 +1,22 @@
 module Advent.Y2020.Day6.Part1 where
 
+import Advent.Input (readInput)
+import Data.List.Split
+import qualified Data.Set as Set
+import Data.Text (unpack)
+
+type Paragraph = [Text]
+
+paragraphs :: [Text] -> [Paragraph]
+paragraphs = splitWhen (== "")
+
+questionsAnswered :: Paragraph -> Int
+questionsAnswered = Set.size . Set.unions . map (Set.fromList . unpack)
+
+solve :: [Text] -> Int
+solve = sum . map questionsAnswered . paragraphs
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  lines <- readInput "src/Advent/Y2020/Day6/input"
+  print $ solve lines
--- src/Advent/Y2020/Day6/Part2.hs
+++ src/Advent/Y2020/Day6/Part2.hs
@@ -1,4 +1,25 @@
 module Advent.Y2020.Day6.Part2 where
 
+import Advent.Input (readInput)
+import Data.List.Split
+import qualified Data.Set as Set
+import Data.Text (unpack)
+
+type Paragraph = [Text]
+
+paragraphs :: [Text] -> [Paragraph]
+paragraphs = splitWhen (== "")
+
+intersections :: Ord a => [Set a] -> Set a
+intersections sets = foldr Set.intersection (Set.unions sets) sets
+
+questionsAnswered :: Paragraph -> Int
+questionsAnswered = Set.size . intersections . map (Set.fromList . unpack)
+
+solve :: [Text] -> Int
+solve = sum . map questionsAnswered . paragraphs
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  lines <- readInput "src/Advent/Y2020/Day6/input"
+  print $ solve lines

c8011054c580f3fb20ef076ff658d15bb6d09082
Saxon Jensen <saxon.jensen@healthengine.com.au>
2020-12-09 12:12:13 +0800

    Finish part 7

--- src/Advent/Y2020/Day7/Part2.hs
+++ src/Advent/Y2020/Day7/Part2.hs
@@ -3,7 +3,6 @@ module Advent.Y2020.Day7.Part2 where
 import Advent.Input (readInput)
 import Data.List.Split
 import qualified Data.Map.Strict as Map
-import Data.Maybe
 import Data.Text (unpack)
 import Relude.Unsafe (read)
 import Text.Regex.TDFA
@@ -29,26 +28,18 @@ parseLine :: String -> (String, [Child])
 parseLine line = (parent, children)
   where
     (_, _, _, [parent, childList]) = line =~ ruleLineFormat :: (String, String, String, [String])
-    children = map parseBagRule $ splitRuleList childList
+    children = filter ((/= "") . fst) $ map parseBagRule $ splitRuleList childList
 
 getChildMap :: [Text] -> Map String [Child]
 getChildMap = Map.fromList . map (parseLine . unpack)
 
-path _ visited [] total = total
-path graph visited (currEl@(curr, cnt) : next) total
-  | curr `elem` visited = total + path graph visited next
-  | otherwise = path graph (curr : visited) (addToList graph currEl next)
-  where
-    addToList graph (curr, _) next = case Map.lookup curr graph of
-      Just ls -> ls ++ next
-      Nothing -> next
-
 bagCount :: Graph -> String -> Int
-bagCount graph bag = sum $ map (\(b, c) -> c * (bagCount graph b)) $ (Map.!) graph bag
+bagCount graph bag = 1 + (sum $ map (\(b, c) -> c * bagCount graph b) childs)
+  where
+    childs = (Map.!) graph bag
 
 solution :: IO ()
 solution = do
   inputLines <- readInput "src/Advent/Y2020/Day7/input"
   let graph = getChildMap inputLines
-  let shinyPath = path graph ["shiny gold"] ((Map.!) graph "shiny gold")
-  print "hello"
+  print $ bagCount graph "shiny gold" - 1

25bf9d3802cc7054a5fc4e3a6e480ebb35caf2ee
Saxon Jensen <saxon.jensen@healthengine.com.au>
2020-12-10 13:28:09 +0800

    Part 1 day 10

--- src/Advent/Y2020/Day10/Part1.hs
+++ src/Advent/Y2020/Day10/Part1.hs
@@ -1,4 +1,22 @@
 module Advent.Y2020.Day10.Part1 where
 
+import Advent.Input
+import Data.List (maximum)
+
+differences :: [Integer] -> [Integer]
+differences xs = case xs of
+  (x : y : z) -> y - x : differences (y : z)
+  [_] -> []
+  _ -> []
+
+solve :: [Integer] -> Int
+solve input = length ones * length threes
+  where
+    gaps = differences $ sort (0 : input ++ [maximum input + 3])
+    ones = filter (== 1) gaps
+    threes = filter (== 3) gaps
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  input <- readIntegers "src/Advent/Y2020/Day10/input"
+  print $ solve input

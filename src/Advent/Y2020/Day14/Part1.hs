b3135cd44b5f29292a43640fdd23eb31b96c33c5
Saxon Jensen <saxon.jensen@healthengine.com.au>
2020-12-22 14:09:11 +0800

    Solve day 14 part 1

--- src/Advent/Y2020/Day14/Part1.hs
+++ src/Advent/Y2020/Day14/Part1.hs
@@ -11,26 +11,37 @@ type Rule = (Integer, Text)
 
 type Program = (Text, [Text])
 
+data Cmd = Mask Text | Mem Rule deriving (Show)
+
 padL :: a -> Int -> [a] -> [a]
 padL p s l
   | length l >= s = l
   | otherwise = replicate (s - length l) p ++ l
 
-ruleRegex = "mem\\[([0-9]+)\\] = ([01]+)" :: String
+ruleRegex = "mem\\[([0-9]+)\\] = ([0-9]+)" :: String
 
 maskRegex = "mask = ([01X]+)" :: String
 
-parseRules :: [Text] -> [Rule]
-parseRules = map parseRule
+stringSize = 36
+
+parseRule :: Text -> Cmd
+parseRule txt =
+  if null mem
+    then Mask (pack mask)
+    else case mem of
+      [address, n] -> Mem (read address, pack $ padL '0' stringSize $ unpack $ encode $ read n)
+      _ -> error "It's fucked"
   where
-    parseRule rule = (read address, pack $ padL '0' 32 $ unpack $ encode $ read num)
-      where
-        (_, _, _, [address, num]) = unpack rule =~ ruleRegex :: (String, String, String, [String])
+    (_, _, _, mem) = unpack txt =~ ruleRegex :: (String, String, String, [String])
+    (_, _, _, [mask]) = unpack txt =~ maskRegex :: (String, String, String, [String])
+
+parseRules :: [Text] -> [Cmd]
+parseRules = map parseRule
 
 decode :: Text -> Integer
 decode = foldr f 0 . reverse . zip [0 ..] . reverse . unpack
   where
-    f (i, c) acc = if c == '0' then 0 else acc + 2 ^ i
+    f (i, c) acc = if c == '0' then acc else acc + 2 ^ i
 
 encode :: Integer -> Text
 encode = pack . map intToDigit . bin
@@ -47,18 +58,17 @@ applyMask mask num = pack $ zipWith result (unpack mask) (unpack num)
       'X' -> x
       _ -> m
 
-runProgram :: Text -> [Rule] -> Map Integer Integer
-runProgram mask rules = foldl' fun Map.empty rules
+runProgram :: [Cmd] -> (Text, Map Integer Integer)
+runProgram rules = foldl' fun ("", Map.empty) rules
   where
-    fun accMap (mem, num) = Map.insert mem newNum accMap
-      where
-        newNum = decode $ applyMask mask num
+    fun (mask, accMap) cmd = case cmd of
+      Mask m -> (m, accMap)
+      Mem (a, n) -> (mask, Map.insert a (decode $ applyMask mask n) accMap)
 
-solve :: Text -> [Text] -> Integer
-solve mask = sum . Map.elems . runProgram mask . parseRules
+solve :: [Text] -> Integer
+solve = sum . Map.elems . snd . runProgram . parseRules
 
 solution :: IO ()
 solution = do
-  (maskLine : rules) <- readInput "src/Advent/Y2020/Day14/test"
-  let (_, _, _, [mask]) = unpack maskLine =~ maskRegex :: (String, String, String, [String])
-  print $ solve (pack mask) rules
+  rules <- readInput "src/Advent/Y2020/Day14/input"
+  print $ solve rules

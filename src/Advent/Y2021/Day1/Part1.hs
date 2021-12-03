ed98c56f272fb761d5e1c4b2e9c7ca651f7fbeba
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-12-04 00:00:10 +0800

    Day 1

--- src/Advent/Y2021/Day1/Part1.hs
+++ src/Advent/Y2021/Day1/Part1.hs
@@ -1,4 +1,18 @@
 module Advent.Y2021.Day1.Part1 where
 
+import Advent.Input
+import Data.List
+import Data.Maybe
+import Prelude hiding (lines)
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  nums <- readIntegers "src/Advent/Y2021/Day1/input.txt"
+  print $ solve nums
+
+solve :: [Integer] -> Integer
+solve (x:y:xs)
+    | x < y = 1 + solve (y:xs)
+    | otherwise = solve (y:xs)
+solve (x:xs) = solve xs
+solve [] = 0

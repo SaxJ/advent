92b60bfee904dc18e2d45b209b8f3f0494d3013f
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-12-03 11:52:15 +0800

    2021 Generated

---
+++ src/Advent/Y2021/Day2/Part1.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Advent.Y2021.Day2.Part1 where
+import Advent.Input
+import Relude.Unsafe (read)
+
+solution :: IO ()
+solution = do
+  lines <- readInput "src/Advent/Y2021/Day2/input"
+  print $ solve $ map toString lines
+
+
+resolveChange :: String -> (Integer, Integer)
+resolveChange cmd = let
+    [dir, amountStr] = words $ toText cmd
+    amount = read $ toString amountStr
+    in
+        case (dir, amount) of
+            ("forward", x) -> (x, 0)
+            ("down", x) -> (0, x)
+            (_, x) -> (0, negate x)
+
+solve :: [String] -> Integer
+solve = prod . foldr (tupleSum . resolveChange) (0, 0)
+    where
+        tupleSum (a,b) (x, y) = (a + x, b + y)
+        prod (x, y) = x * y

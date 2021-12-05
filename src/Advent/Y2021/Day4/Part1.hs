ef5c892d61964eac0232da5c3a61652e9fdd9ee7
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-12-06 01:02:23 +0800

    Ugh

--- src/Advent/Y2021/Day4/Part1.hs
+++ src/Advent/Y2021/Day4/Part1.hs
@@ -15,21 +15,21 @@ falseMatrix = M.fromList 5 5 $ repeat False
 
 -- Runs through commands accumulating operation matrices
 accumulateOperations :: [Integer] -> M.Matrix Integer -> [M.Matrix Bool]
-accumulateOperations cmds mtx = foldl accumulator [] cmds
+accumulateOperations cmds mtx = foldl accumulator [falseMatrix] cmds
   where
     accumulator ms cmd = ms ++ [next]
       where
         next = M.elementwise (||) (matrixOp mtx cmd) (fromJust $ viaNonEmpty last ms)
 
 matrixOp :: M.Matrix Integer -> Integer -> M.Matrix Bool
-matrixOp mtx val = case idx of
+matrixOp mtx val = case midx of
   Just i -> let
     r = i `div` M.ncols mtx
     c = i `mod` M.nrows mtx
-    in M.setElem True (r, c) falseMatrix
+    in trace (show (i, r, c)) fromMaybe falseMatrix $ M.safeSet True (r, c) falseMatrix
   _ -> falseMatrix
   where
-    idx = elemIndex val $ M.toList mtx
+    midx = elemIndex val $ M.toList mtx
 
 matricesFromInput :: [String] -> [M.Matrix Integer]
 matricesFromInput = map makeMatrix . matrixChunks
@@ -46,8 +46,14 @@ readCommands = map read . splitOn "," . fromJust . viaNonEmpty head
 getInputs :: [String] -> [String]
 getInputs = filter (not . null)
 
+solve lines = map (accumulateOperations cmds) matrices
+  where
+    inputs = getInputs $ map toString lines
+    cmds = readCommands inputs
+    matrices = matricesFromInput inputs
+
+
 solution :: IO ()
 solution = do
   lines <- readInput "src/Advent/Y2021/Day4/input.test"
-  let inputs = getInputs $ map toString lines
-  print "hello"
+  print $ solve lines

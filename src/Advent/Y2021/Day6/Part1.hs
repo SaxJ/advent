833e800f86144e8dad25de58442efb895e91a063
Saxon Jensen <saxon.jensen@healthengine.com.au>
2021-12-12 20:22:09 +0800

    Day 6 part 1

--- src/Advent/Y2021/Day6/Part1.hs
+++ src/Advent/Y2021/Day6/Part1.hs
@@ -1,4 +1,21 @@
 module Advent.Y2021.Day6.Part1 where
+import Advent.Input (readInput)
+import Data.Text (unpack, pack, splitOn)
+import Debug.Trace (trace)
+
+step :: [Integer] -> [Integer]
+step input = map dec input ++ replicate zs 8
+  where
+    zs = length $ filter (== 0) input
+    dec x
+      | x == 0 = 6
+      | otherwise = x - 1
+
+solve input = length $ foldr (\_ acc -> step acc) start [1..256]
+  where
+    start = [read x | x <- map unpack $ splitOn "," $ pack input]
 
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  lines <- readInput "src/Advent/Y2021/Day6/input"
+  print $ solve $ head $ map unpack lines

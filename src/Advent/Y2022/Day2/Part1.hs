c75b4b363f039eb1d0faf5be8afa199c6c3186db
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-12-03 00:47:10 +0800

    2022 day 2

--- src/Advent/Y2022/Day2/Part1.hs
+++ src/Advent/Y2022/Day2/Part1.hs
@@ -1,4 +1,51 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Advent.Y2022.Day2.Part1 where
 
+import Advent.Input (readInput)
+import Data.Text hiding (map)
+import GHC.Float (expts10)
+import Prelude hiding (words, head)
+import Data.Char (GeneralCategory(ClosePunctuation))
+
+-- x lose
+-- y draw
+-- z win
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  lines <- readInput "src/Advent/Y2022/Day2/input"
+  print $ solve lines
+
+solve :: [Text] -> Int
+solve lines = sum $ map pairToScore pairs
+  where
+    pairs = map lineToPair lines
+
+lineToPair :: Text -> (Char, Char)
+lineToPair l = (x, y)
+  where
+    [xs, ys] = words l
+    x = head xs
+    y = head ys
+
+pairToScore :: (Char, Char) -> Int
+pairToScore p@(a, b) = x + y
+  where
+    x = case b of
+          'X' -> 1
+          'Y' -> 2
+          'Z' -> 3
+          _ -> 0
+    y = case p of
+          ('A', 'X') -> 3
+          ('B', 'Y') -> 3
+          ('C', 'Z') -> 3
+
+          ('A', 'Y') -> 6
+          ('B', 'Z') -> 6
+          ('C', 'X') -> 6
+
+          ('A', 'Z') -> 0
+          ('B', 'X') -> 0
+          ('C', 'Y') -> 0
+          _ -> 0
--- src/Advent/Y2022/Day2/Part2.hs
+++ src/Advent/Y2022/Day2/Part2.hs
@@ -1,4 +1,50 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Advent.Y2022.Day2.Part2 where
 
+import Advent.Input (readInput)
+import Data.Text hiding (map)
+import Prelude hiding (words, head)
+import Data.Char (GeneralCategory(ClosePunctuation))
+
+
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+  lines <- readInput "src/Advent/Y2022/Day2/input"
+  print $ solve lines
+
+solve :: [Text] -> Int
+solve lines = sum $ map pairToScore pairs
+  where
+    pairs = map lineToPair lines
+
+lineToPair :: Text -> (Char, Char)
+lineToPair l = (x, y)
+  where
+    [xs, ys] = words l
+    x = head xs
+    y = head ys
+
+pairToScore :: (Char, Char) -> Int
+pairToScore p@(a, b) = x + y
+  where
+        -- x lose
+        -- y draw
+        -- z win
+    x = case b of
+          'X' -> 0
+          'Y' -> 3
+          'Z' -> 6
+          _ -> 0
+    y = case p of
+          ('A', 'X') -> 3
+          ('B', 'X') -> 1
+          ('C', 'X') -> 2
+
+          ('A', 'Y') -> 1
+          ('B', 'Y') -> 2
+          ('C', 'Y') -> 3
+
+          ('A', 'Z') -> 2
+          ('B', 'Z') -> 3
+          ('C', 'Z') -> 1
+          _ -> 0

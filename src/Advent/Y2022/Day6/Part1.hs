d0cc8474eefc05c77125a5d975c85e890dca25e9
Saxon Jensen <saxon.jensen@healthengine.com.au>
2022-12-06 22:45:48 +0800

    Solved day 6

--- src/Advent/Y2022/Day6/Part1.hs
+++ src/Advent/Y2022/Day6/Part1.hs
@@ -1,4 +1,24 @@
 module Advent.Y2022.Day6.Part1 where
+import Data.List (tails, nub)
+import Data.Text (unpack)
+import Advent.Input (readInput)
+import Data.List (findIndex)
+import Data.Maybe (fromMaybe)
 
 solution :: IO ()
-solution = putStrLn "Not implemented yet"
+solution = do
+    lines <- readInput "src/Advent/Y2022/Day6/input"
+    print $ solve $ head $ map unpack lines
+
+solve :: String -> Int
+solve s = case mi of
+            Nothing -> (-1)
+            Just i -> 4 + i
+    where
+        mi = findIndex uniqueWindow $ windows 4 s
+
+uniqueWindow :: String -> Bool
+uniqueWindow s = (length $ nub s) == 4
+
+windows :: Int -> [a] -> [[a]]
+windows m = foldr (zipWith (:)) (repeat []) . take m . tails
